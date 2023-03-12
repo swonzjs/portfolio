5b4ee00d97e0af05cdbb089848211152846a95ab
Swonz <82213751+swonzjs@users.noreply.github.com>
2023-03-12 21:37:37 +0300

    Update clqu.config.js

--- clqu.config.js
+++ clqu.config.js
@@ -6,7 +6,7 @@ module.exports = {
     softwareerKey: process.env.SOFTWAREER_KEY,
     email: 'flx.dev@yandex.com',
     errors: {
-        404: "This page could not be found..",
+        404: "This page could not be found.",
         500: "An error occurred while processing your request."
     }
 } 
