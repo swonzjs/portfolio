68d1727a91371e9d19dba7eee607da737e989ea1
Swonz <82213751+swonzjs@users.noreply.github.com>
2023-03-12 21:40:21 +0300

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -31,7 +31,7 @@ export default function Home() {
         </div>
         <div className="rounded-lg border-2 border-gray-500/10 border-dotted px-4 py-2 text-center">
           <span className="text-primary uppercase">Months of Experience</span>
-          <p className="text-gray-500 dark:text-gray-300 font-semibold text-xl">4++</p>
+          <p className="text-gray-500 dark:text-gray-300 font-semibold text-xl">4+</p>
         </div>
         <div className="rounded-lg border-2 border-gray-500/10 border-dotted px-4 py-2 text-center">
           <span className="text-primary uppercase">Completed Projects</span>
